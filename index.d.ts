61d3e0f598512b0d99ea71db4f5af5f239427c3f
Manuel Warum <mwarum@gmail.com>
2019-10-21 11:23:34 +0200

    Update index.d.ts
    Changing indentations

--- index.d.ts
+++ index.d.ts
@@ -1,19 +1,24 @@
 declare module 'keycharm' {
-    interface KeycharmOptions {
-        preventDefault?: boolean;
-        container?: Window | Element;
-    }
+  interface KeycharmOptions {
+    preventDefault?: boolean;
+    container?: Window | Element;
+  }
 
-    type BindType = 'keydown' | 'keyup';
+  type BindType = 'keydown' | 'keyup';
 
-    interface Keycharm {
-        bind (key: string, callback: (ev: KeyboardEvent) => void, type?: BindType): void;
-        bindAll (callback: (ev: KeyboardEvent) => void, type?: BindType): void;
-        unbind (key: string, callback?: (ev: KeyboardEvent) => void, type?: BindType): void;
-        reset (): void;
-        destroy (): void
-        getKey (event: KeyboardEvent): string;
-    }
+  interface Keycharm {
+    bind(key: string, callback: (ev: KeyboardEvent) => void, type?: BindType): void;
 
-    export default function keycharm (options?: KeycharmOptions): Keycharm;
+    bindAll(callback: (ev: KeyboardEvent) => void, type?: BindType): void;
+
+    unbind(key: string, callback?: (ev: KeyboardEvent) => void, type?: BindType): void;
+
+    reset(): void;
+
+    destroy(): void
+
+    getKey(event: KeyboardEvent): string;
+  }
+
+  export default function keycharm(options?: KeycharmOptions): Keycharm;
 }
